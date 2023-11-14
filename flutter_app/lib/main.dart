30a38b22555132925dcdcdec73ce603c3c54eea2
aigerim tuishieva <gerardinearmstrong@gmail.com>
2023-11-15 05:51:32 +0600

    version 9

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -13,6 +13,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: ExampleDragAndDrop());
+        debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
